ff5fb4cc5df18e01fd1dcdf3d4af8fabbb274887
Kozuki <kozuki1987@gmail.com>
2022-12-26 23:06:54 +1300

    add GameState and init game

--- GameMain.java
+++ GameMain.java
@@ -27,9 +27,9 @@ public class GameMain extends JPanel implements MouseListener{
 	// the game board 
 	private Board board;
 	 	 
-	//TODO: create the enumeration for the variable below (GameState currentState)
+	//Done: create the enumeration for the variable below (GameState currentState)
 	//HINT all of the states you require are shown in the code within GameMain
-	private GameState currentState; 
+	private GameState currentState;
 	
 	// the current player
 	private Player currentPlayer; 
@@ -40,7 +40,8 @@ public class GameMain extends JPanel implements MouseListener{
 	/** Constructor to setup the UI and game components on the panel */
 	public GameMain() {   
 		
-		// TODO: This JPanel fires a MouseEvent on MouseClicked so add required event listener to 'this'.          
+		// TODO: This JPanel fires a MouseEvent on MouseClicked so add required event listener to 'this'.
+		addMouseListener(this);
 	    
 	    
 		// Setup the status bar (JLabel) to display status message       
@@ -57,10 +58,12 @@ public class GameMain extends JPanel implements MouseListener{
 		setPreferredSize(new Dimension(CANVAS_WIDTH, CANVAS_HEIGHT + 30));
 		
 		
-		// TODO: Create a new instance of the game "Board"class. HINT check the variables above for the correct name
+		// Done: Create a new instance of the game "Board"class. HINT check the variables above for the correct name
+		board = new Board();
 
 		
 		//TODO: call the method to initialise the game board
+		initGame();
 
 	}
 	
@@ -71,11 +74,12 @@ public class GameMain extends JPanel implements MouseListener{
 				//create a main window to contain the panel
 				JFrame frame = new JFrame(TITLE);
 				
-				//TODO: create the new GameMain panel and add it to the frame
+				//Done: create the new GameMain panel and add it to the frame
+				 frame.add(new GameMain());
 
 				
 				
-				//TODO: set the default close operation of the frame to exit_on_close
+				//Done: set the default close operation of the frame to exit_on_close
 				 frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		            
 				
@@ -160,7 +164,8 @@ public class GameMain extends JPanel implements MouseListener{
 		 *  UpdateGame is called which will call the methods to check for winner or Draw. if none then GameState remains playing.
 		 *  If win or Draw then call is made to method that resets the game board.  Finally a call is made to refresh the canvas so that new symbol appears*/
 	
-	public void mouseClicked(MouseEvent e) {  
+	public void mouseClicked(MouseEvent e) {
+		System.out.println("on mouse clicked");
 	    // get the coordinates of where the click event happened            
 		int mouseX = e.getX();             
 		int mouseY = e.getY();             
